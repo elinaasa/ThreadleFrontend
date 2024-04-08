54667cfdbb7b88897b694f0a51cab88483e72af1
Elina Sankila <sankilaelina@gmail.com>
2024-04-08 15:44:07 +0300

    home component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
+import Home from './components/Home';
+
 const App = () => {
+
   return (
+
     <>
-      <h1>My App</h1>
+      <Home />
     </>
   );
 };
---
+++ my-app/src/components/MediaRow.tsx
@@ -0,0 +1,19 @@
+import {MediaItem} from "../types/DBtypes";
+
+const MediaRow = (props: {mediaItem: MediaItem}) => {
+  const item = props.mediaItem;
+  return (
+    <tr className="media-row">
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+    </tr>
+  );
+};
+
+export default MediaRow;
